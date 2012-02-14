351e5b16d9923902135f4f9ef395b492d68feac7
Andreas Schempp <andreas@schempp.ch>
2012-02-14 15:38:31 +0100

    Added missing fields to database.sql

--- config/database.sql
+++ config/database.sql
@@ -40,6 +40,8 @@ CREATE TABLE `tl_dataexchange_fields` (
   `sorting` int(10) unsigned NOT NULL default '0',
   `tstamp` int(10) unsigned NOT NULL default '0',
   `dcaField` varchar(255) NOT NULL default '',
+  `label` varchar(255) NOT NULL default '',
+  `fieldQuery` varchar(255) NOT NULL default '',
   `enabled` char(1) NOT NULL default '',
   PRIMARY KEY  (`id`),
   KEY `pid` (`pid`),
